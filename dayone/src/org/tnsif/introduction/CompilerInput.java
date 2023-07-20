a644fdfd47de79dbd4190848ee3f1709d6b87928
DELL <DELL@DESKTOP-5Q1V3V4>
2023-07-20 14:10:24 +0530

    Commit one

---
+++ dayone/src/org/tnsif/introduction/CompilerInput.java
@@ -0,0 +1,13 @@
+//program to demonstrate program on compiletime input
+package org.tnsif.introduction;
+
+public class CompilerInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short num =34;
+		System.out.println("The num is"+num);
+
+	}
+
+}
---
+++ dayone/src/org/tnsif/introduction/UserInput_Demo.java
@@ -0,0 +1,18 @@
+//program to demostarate user input
+package org.tnsif.introduction;
+
+import java.util.Scanner;
+
+public class UserInput_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s =new Scanner(System.in);
+		System.out.println("Enter a string:");
+		String strl=s.nextLine();
+		System.out.println("String is :"+S);
+				
+
+	}
+
+}
